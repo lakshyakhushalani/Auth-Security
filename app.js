c7a31c8ed1de9981a48a0a8848df90fe0a5c0c77
lakshya <lakshyakhushalani98@gmail.com>
2023-01-07 17:06:01 +0530

    till 2nd level encryption

---
+++ app.js
@@ -0,0 +1,76 @@
+const express=require("express");
+const ejs=require("ejs");
+const bodyparser=require("body-parser");
+const mongoose=require("mongoose");
+const  encrypt=require('mongoose-encryption');
+const app=express();
+const Schema=mongoose.Schema;
+
+mongoose.set('strictQuery', true)
+
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
+//DB connection
+mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser:true});
+
+//Schema define
+const xxx=new Schema(
+  {
+    email:String,
+    password:String
+  }
+)
+
+//encrypting password  --this must be done beofre making a collection using mongoose.model
+const secret="thisisourlittlesecret";
+xxx.plugin(encrypt,{ secret:secret, encryptedFields : ["password"] });
+
+//making collection
+const users=new mongoose.model("users",xxx);
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+  res.render("home");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+
+app.post("/register",(req,res)=>{
+  const Email=req.body.username;
+  const Password=req.body.password;
+
+  const newuser=new users({
+    email:Email,
+    password:Password
+  });
+  newuser.save( (err)=>{
+    if(err) res.send("Not Register");
+    else res.render("secrets");
+  } )
+
+})
+
+app.post("/login",(req,res)=>{
+  const Email=req.body.username;
+  const Password=req.body.password;
+
+
+  users.findOne({email:Email},(err,found)=>{
+    if(err) console.log(err);
+    else
+     {
+        if(found){if(found.password===Password) res.render("secrets");
+        else res.send("Incorrect Password");}
+     }
+  })
+})
+
+app.listen(3000,"localhost",()=>{console.log("server is listening to the port 3000");});
